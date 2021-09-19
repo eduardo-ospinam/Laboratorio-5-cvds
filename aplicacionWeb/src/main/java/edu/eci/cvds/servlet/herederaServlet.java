bb818c206a1313d806acbaa0f73d8b37ef0e8dab
Deivid <deivid.medina@mail.escuelaing.edu.co>
2021-09-19 15:31:30 -0500

    Segunda parte, puntos 12,13,14 y 15

--- aplicacionWeb/src/main/java/edu/eci/cvds/servlet/herederaServlet.java
+++ aplicacionWeb/src/main/java/edu/eci/cvds/servlet/herederaServlet.java
@@ -1,7 +1,13 @@
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Optional;
+package edu.eci.cvds.servlet;
 
+import edu.eci.cvds.servlet.model.Todo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +24,36 @@ public class herederaServlet extends HttpServlet {
 		Writer responseWriter = resp.getWriter();
 	    Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
 		String parametroId = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
-		       
-		 }
+        if(parametroId != "" ){
+            try{
+                int numeroId = Integer.parseInt(parametroId);
+                if(numeroId<=200 && numeroId >=1 ){
+                    Todo todo = Service.getTodo(numeroId);
+                    List<Todo> todoList = new ArrayList<>();
+                    todoList.add(todo);
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.setContentType("text/html"); //ignifica qué tipo de respuesta desea enviar al cliente,
+                    responseWriter.write(Service.todosToHTMLTable(todoList));
+                    responseWriter.flush();
+                }else{
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND); //SC_NOT_FOUND: código de estado (404) que indica que el recurso solicitado no está disponible.
+                    responseWriter.write("No existe un item con el identificador dado.");
+                    responseWriter.flush();
+                }
+            }catch(MalformedURLException e){
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); //Código de estado (500) que indica un error dentro del servidor HTTP que le impidió cumplir con la solicitud.
+                responseWriter.write("Error interno del servidor.");
+                responseWriter.flush();
+            }catch (Exception e){
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);//400: bad request, esta respuesta siginifica que el servidor no pudo interpretar la solicitud dada
+                responseWriter.write("Requerimiento inválido.");
+                responseWriter.flush();
+            }
+            
+        }else{
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseWriter.write("Requerimiento inválido.");
+            responseWriter.flush();
+        }   
+    }
 }
