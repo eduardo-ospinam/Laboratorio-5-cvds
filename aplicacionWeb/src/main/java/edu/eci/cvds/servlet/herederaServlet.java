b56d0ecab3ca2186266b5cd6631668ebcc2de6ec
Deivid <deivid.medina@mail.escuelaing.edu.co>
2021-09-19 15:42:25 -0500

    Partes 3, primer punto.

--- aplicacionWeb/src/main/java/edu/eci/cvds/servlet/herederaServlet.java
+++ aplicacionWeb/src/main/java/edu/eci/cvds/servlet/herederaServlet.java
@@ -19,11 +19,53 @@ import javax.servlet.http.HttpServletResponse;
 	)
 
 public class herederaServlet extends HttpServlet {
+	
+	//doGet () se utilizará cuando una pequeña cantidad de datos y datos insensibles, como una consulta
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Writer responseWriter = resp.getWriter();
 	    Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
 		String parametroId = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+        if(parametroId != "" ){
+            try{
+                int numeroId = Integer.parseInt(parametroId);
+                if(numeroId<=200 && numeroId >=1 ){
+                    Todo todo = Service.getTodo(numeroId);
+                    List<Todo> todoList = new ArrayList<>();
+                    todoList.add(todo);
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.setContentType("text/html"); //ignifica qué tipo de respuesta desea enviar al cliente,
+                    responseWriter.write(Service.todosToHTMLTable(todoList));
+                    responseWriter.flush();
+                }else{
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND); //SC_NOT_FOUND: código de estado (404) que indica que el recurso solicitado no está disponible.
+                    responseWriter.write("No existe un item con el identificador dado.");
+                    responseWriter.flush();
+                }
+            }catch(MalformedURLException e){
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); //Código de estado (500) que indica un error dentro del servidor HTTP que le impidió cumplir con la solicitud.
+                responseWriter.write("Error interno del servidor.");
+                responseWriter.flush();
+            }catch (Exception e){
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);//400: bad request, esta respuesta siginifica que el servidor no pudo interpretar la solicitud dada
+                responseWriter.write("Requerimiento inválido.");
+                responseWriter.flush();
+            }
+            
+        }else{
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseWriter.write("Requerimiento inválido.");
+            responseWriter.flush();
+        }   
+    }
+	
+	// doPost () se utilizará cuando se deba enviar una cantidad relativamente grande de datos confidenciales.
+	@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Writer responseWriter = resp.getWriter();
+	    Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+		String parametroId = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+        
         if(parametroId != "" ){
             try{
                 int numeroId = Integer.parseInt(parametroId);
